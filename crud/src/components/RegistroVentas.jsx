103d24444e0964f9ed7b61f592f2d0238c751224
bairontruano <amongrimorio@javerianacali.edu.co>
2024-09-21 18:27:28 -0500

    Agregar multiples productos

--- crud/src/components/RegistroVentas.jsx
+++ crud/src/components/RegistroVentas.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 function RegistroVentas() {
     const apiUrl = import.meta.env.VITE_API_URL;
 
-    const [nombreProducto, setNombreProducto] = useState('')
+    const [nombreProducto, setNombreProducto] = useState('');
     const [producto, setProducto] = useState('');
     const [cantidad, setCantidad] = useState('');
     const [maximo, setMaximo] = useState();
@@ -15,6 +15,7 @@ function RegistroVentas() {
     const [medioPago, setMedioPago] = useState('');
     const [idBanco, setIdBanco] = useState('');
     const [productos, setProductos] = useState([]);
+    const [productosVenta, setProductosVenta] = useState([]); // Almacenar múltiples productos
     const [empleados, setEmpleados] = useState([]);
     const [bancos, setBancos] = useState([]);
     const [clienteData, setClienteData] = useState(0);
@@ -53,26 +54,36 @@ function RegistroVentas() {
         const productoSeleccionado = productos.find(prod => prod.IdProducto === parseInt(e.target.value));
         setProducto(e.target.value);
         if (productoSeleccionado) {
-            setNombreProducto(productoSeleccionado.NombreProducto)
+            setNombreProducto(productoSeleccionado.NombreProducto);
             setPrecioUnitario(productoSeleccionado.ValorUnidad);
-            setMaximo(productoSeleccionado.Cantidad)
+            setMaximo(productoSeleccionado.Cantidad);
         } else {
             setPrecioUnitario('');
         }
     };
 
     const agregarProducto = () => {
-        if (producto && cantidad && precioUnitario) {
+        if (producto && cantidad && precioUnitario && cantidad <= maximo) {
             const nuevoProducto = {
                 IdProducto: producto,
+                NombreProducto: nombreProducto,
                 Cantidad: cantidad,
-                PrecioUnitario: precioUnitario
+                PrecioUnitario: precioUnitario,
+                Total: cantidad * precioUnitario // Calcular el total por producto
             };
             setProductosVenta([...productosVenta, nuevoProducto]);
             setProducto('');
             setCantidad('');
             setPrecioUnitario('');
+            setMaximo(0);
+        } else {
+            alert("Verifica que hayas seleccionado un producto, una cantidad válida y que esté disponible en inventario.");
         }
+    };
+
+    const eliminarProducto = (index) => {
+        const nuevosProductosVenta = productosVenta.filter((_, i) => i !== index);
+        setProductosVenta(nuevosProductosVenta);
     };
 
     const handleSubmit = async (e) => {
@@ -87,9 +98,6 @@ function RegistroVentas() {
             };
 
             const venta = {
-                //IdProducto: producto,
-                //Cantidad: cantidad,
-                //PrecioUnitario: precioUnitario,
                 IdCliente: cliente,
                 IdEmpleado: empleado,
                 Fecha: fechaActual,
@@ -97,39 +105,40 @@ function RegistroVentas() {
                 IdBanco: medioPago === 'Tarjeta' || medioPago === 'Transferencia' ? idBanco : 4
             };
 
-            if(!clienteData.NombreCliente){
-                
-                await axios.post(`${apiUrl}/api/cliente/`, nuevoCliente)
-                console.log("Cliente creado correctamente")
+            if (!clienteData.NombreCliente) {
+                await axios.post(`${apiUrl}/api/cliente/`, nuevoCliente);
+                console.log("Cliente creado correctamente");
             }
-
 
             const response = await axios.post(`${apiUrl}/api/factura/`, venta);
 
-            const ProductoFactura = {
-                IdFactura: response.data.IdFactura,
-                IdProducto: parseInt(producto),
-                Cantidad: cantidad
-            }
-            console.log(ProductoFactura)
-            await axios.post(`${apiUrl}/api/productofactura/`, ProductoFactura);
-            const nuevaCantidad = maximo-cantidad
-            await axios.patch(`${apiUrl}/api/producto/${producto}/`, {
-                Cantidad: nuevaCantidad
-              },{
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              });
-            
-            if(nuevaCantidad < 1){
-                await axios.post(`${apiUrl}/api/enviar-email/`, {
-                  destinatario: "juanrx1904@gmail.com",
-                  asunto: "Inventario agotado",
-                  email: `El producto ${nombreProducto} se ha agotado actualmente en el inventario.`,
+            for (const prodVenta of productosVenta) {
+                const productoFactura = {
+                    IdFactura: response.data.IdFactura,
+                    IdProducto: parseInt(prodVenta.IdProducto),
+                    Cantidad: prodVenta.Cantidad
+                };
+                console.log(productoFactura);
+                await axios.post(`${apiUrl}/api/productofactura/`, productoFactura);
+
+                const nuevaCantidad = maximo - prodVenta.Cantidad;
+                await axios.patch(`${apiUrl}/api/producto/${prodVenta.IdProducto}/`, {
+                    Cantidad: nuevaCantidad
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 });
+
+                if (nuevaCantidad < 1) {
+                    await axios.post(`${apiUrl}/api/enviar-email/`, {
+                        destinatario: "juanrx1904@gmail.com",
+                        asunto: "Inventario agotado",
+                        email: `El producto ${prodVenta.NombreProducto} se ha agotado actualmente en el inventario.`,
+                    });
+                }
             }
-              
+
             console.log('Venta registrada:', response.data);
 
             // Restablecer el formulario después de la venta
@@ -140,6 +149,7 @@ function RegistroVentas() {
             setMaximo();
             setClienteData(0);
             setNombreProducto('');
+            setProductosVenta([]); // Vaciar la lista de productos
         } catch (error) {
             console.error('Error registrando la venta:', error);
         }
@@ -149,110 +159,99 @@ function RegistroVentas() {
         e.preventDefault();
         try {
             const response = await axios.get(`${apiUrl}/api/cliente/${cliente}/`);
-            setClienteData(response.data)
+            setClienteData(response.data);
         } catch (error) {
-            setClienteData([])
+            setClienteData([]);
             console.error('Cliente no encontrado:', error);
         }
-
     };
 
-    function renderizarFormulario(){
-        if(clienteData !== 0){
-        return(
-            <>
-            {
-                clienteData.NombreCliente ? 
+    function renderizarFormulario() {
+        if (clienteData !== 0) {
+            return (
                 <>
-                    <h3>{clienteData.NombreCliente}</h3>
-                </>
-                :
-                <>
+                    {clienteData.NombreCliente ? 
+                        <h3>{clienteData.NombreCliente}</h3> : 
+                        <>
+                            <div className="form-group">
+                                <h3>Cliente no encontrado</h3>
+                                <label>Nombre del cliente:</label>
+                                <input
+                                    type="text"
+                                    value={nombre}
+                                    onChange={(e) => setNombre(e.target.value)}
+                                    required
+                                />
+                                <label>Dirección del cliente:</label>
+                                <input
+                                    type="text"
+                                    value={direccion}
+                                    onChange={(e) => setDireccion(e.target.value)}
+                                    required
+                                />
+                            </div>
+                        </>
+                    }
                     <div className="form-group">
-                        <h3>Cliente no encontrado</h3>
-                        <label>Nombre del cliente:</label>
-                        <input
-                            type="text"
-                            value={nombre}
-                            onChange={(e) => setNombre(e.target.value)}
+                        <label>Empleado:</label>
+                        <select
+                            value={empleado}
+                            onChange={(e) => setEmpleado(e.target.value)}
                             required
-                        />
-                        <label>Dirección del cliente:</label>
-                        <input
-                            type="text"
-                            value={direccion}
-                            onChange={(e) => setDireccion(e.target.value)}
-                            required
-                        />
+                        >
+                            <option value="">Selecciona un empleado</option>
+                            {empleados.map(emp => (
+                                <option key={emp.IdEmpleado} value={emp.IdEmpleado}>
+                                    {emp.NombreEmpleado}
+                                </option>
+                            ))}
+                        </select>
+                        {empleado && (
+                            <span className="empleado-id">ID Empleado: {empleado}</span>
+                        )}
                     </div>
+                    <div className="form-group">
+                        <label>Método de Pago:</label>
+                        <select
+                            value={medioPago}
+                            onChange={(e) => setMedioPago(e.target.value)}
+                            required
+                        >
+                            <option value="">Selecciona un método de pago</option>
+                            <option value="Efectivo">Efectivo</option>
+                            <option value="Tarjeta">Tarjeta</option>
+                            <option value="Transferencia">Transferencia</option>
+                        </select>
+                    </div>
+                    {(medioPago === 'Tarjeta' || medioPago === 'Transferencia') && (
+                        <div className="form-group">
+                            <label>Banco:</label>
+                            <select
+                                value={idBanco}
+                                onChange={(e) => setIdBanco(e.target.value)}
+                                required
+                            >
+                                <option value="">Selecciona un banco</option>
+                                {bancos.map(banco => (
+                                    <option key={banco.IdBanco} value={banco.IdBanco}>
+                                        {banco.NombreBanco}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    )}
+                    
+                    <p>Total: {productosVenta.reduce((acc, prod) => acc + prod.Total, 0)}</p>
+                    
+                    <button type="submit" className="submit-button">Registrar Venta</button>
                 </>
-            }
-            <div className="form-group">
-                <label>Empleado:</label>
-                <select
-                    value={empleado}
-                    onChange={(e) => setEmpleado(e.target.value)}
-                    required
-                >
-                    <option value="">Selecciona un empleado</option>
-                    {empleados.map(emp => (
-                        <option key={emp.IdEmpleado} value={emp.IdEmpleado}>
-                            {emp.NombreEmpleado}
-                        </option>
-                    ))}
-                </select>
-                {empleado && (
-                    <span className="empleado-id">ID Empleado: {empleado}</span>
-                )}
-            </div>
-            <div className="form-group">
-                <label>Método de Pago:</label>
-                <select
-                    value={medioPago}
-                    onChange={(e) => setMedioPago(e.target.value)}
-                    required
-                >   
-                    <option value="">Selecciona un método de pago</option>
-                    <option value="Efectivo">Efectivo</option>
-                    <option value="Tarjeta">Tarjeta</option>
-                    <option value="Transferencia">Transferencia</option>
-                </select>
-            </div>
-            {(medioPago === 'Tarjeta' || medioPago == 'Transferencia') && (
-                <div className="form-group">
-                    <label>Banco:</label>
-                    <select
-                        value={idBanco}
-                        onChange={(e) => setIdBanco(e.target.value)}
-                        required
-                    >
-                        <option value="">Selecciona un banco</option>
-                        {bancos.map(banco => (
-                            <option key={banco.IdBanco} value={banco.IdBanco}>
-                                {banco.NombreBanco}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            )}
-
-            {/* Mostrar el total calculado */}
-            <p>Total: {total}</p>
-            
-            <button type="submit" className="submit-button">Registrar Venta</button>
-        </>
-        );
+            );
+        }
     }
-    }
-
-    // Calcular el total (cantidad * precio unitario)
-    const total = cantidad && precioUnitario ? (cantidad * precioUnitario) : 0;
 
     return (
         <div className="registro-ventas-container">
             <h2>Registrar Venta</h2>
-            
-            {/* Mostrar la fecha actual */}
             <p>Fecha Actual: {fechaActual}</p>
             
             <form onSubmit={handleSubmit}>
@@ -264,11 +263,9 @@ function RegistroVentas() {
                         required
                     >
                         <option value="">Selecciona un producto</option>
-                        {productos
-                        .filter(prod => prod.Cantidad > 0) // Filtrar productos con cantidad mayor a 0
-                        .map(prod => (
+                        {productos.filter(prod => prod.Cantidad > 0).map(prod => (
                             <option key={prod.IdProducto} value={prod.IdProducto}>
-                            {prod.NombreProducto}
+                                {prod.NombreProducto}
                             </option>
                         ))}
                     </select>
@@ -300,8 +297,20 @@ function RegistroVentas() {
                         readOnly
                     />
                 </div>
+                <button type="button" onClick={agregarProducto}>Agregar Producto</button>
+
+                <h3>Productos en la factura:</h3>
+                <ul>
+                    {productosVenta.map((prod, index) => (
+                        <li key={index}>
+                            {prod.NombreProducto} - Cantidad: {prod.Cantidad} - Precio: {prod.PrecioUnitario} - Total: {prod.Total}
+                            <button type="button" onClick={() => eliminarProducto(index)}>Eliminar</button>
+                        </li>
+                    ))}
+                </ul>
+
                 <div className="form-group">
-                    <label>Cedula del cliente:</label>
+                    <label>Cédula del cliente:</label>
                     <input
                         type="text"
                         value={cliente}
@@ -309,7 +318,7 @@ function RegistroVentas() {
                         required
                     />
                 </div>
-                <button onClick={buscarCliente} className='boton-buscar'>Buscar cliente</button>
+                <button onClick={buscarCliente} className="boton-buscar">Buscar cliente</button>
                 {renderizarFormulario()}
             </form>
         </div>
